8268e127ba63058d6b42f375421f2f21cb65072b
infoomics <sbx_94@hotmail.com>
2023-01-30 10:43:35 +0000

    added tests for default constructor

--- __tests__/2-oop-vending-machine.test.js
+++ __tests__/2-oop-vending-machine.test.js
@@ -1,5 +1,10 @@
 const VendingMachine = require("../sections/2-oop-vending-machine.js");
 
-describe("function name here...", () => {
-  test("first test here...", () => {});
+describe("class VendingMachine", () => {
+  test("default constructor, getters and setters", () => {
+    const testMachine = new VendingMachine();
+    
+    expect(testMachine.credit).toBe(0);
+    expect(testMachine.stock).toEqual({ A : {}, B : {}, C : {} });
+  });
 });

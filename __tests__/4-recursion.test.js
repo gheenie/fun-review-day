1dbe101a0e44e96659b65b052146c5dc16e7dcaf
infoomics <sbx_94@hotmail.com>
2023-01-30 13:39:11 +0000

    added tests and code for deepEntries()

--- __tests__/4-recursion.test.js
+++ __tests__/4-recursion.test.js
@@ -4,6 +4,86 @@ const {
   flat,
 } = require("../sections/4-recursion.js");
 
-describe("function name here...", () => {
-  test("first test here...", () => {});
+describe("deepEntries()", () => {
+  test("flat object; 1 item", () => {
+    expect( deepEntries({ name: "Sam" }) ).toEqual([ ["name", "Sam"] ]);
+  });
+  
+  test("flat object; >1 items", () => {
+    expect( deepEntries({ name: "Sam", favBook: "Blood Meridian" }) ).toEqual([ ["name", "Sam"], ["favBook", "Blood Meridian"] ]);
+  });
+  
+  test("1 nest level object", () => {
+    const output = deepEntries({ 
+      name: "Sam", 
+      pets: { 
+        name: "fido" 
+      } 
+    });
+
+    const expected = [
+      ["name", "Sam"],
+      ["pets", [
+        ["name", "fido"]
+      ]]
+    ];
+
+    expect(output).toEqual(expected);
+  });
+
+  test(">1 nest level object", () => {
+    const output = deepEntries({
+      name: "Sam",
+      favBook: { 
+        title: "Blood Meridian", 
+        author: { 
+          name: "Cormac McCarthy" 
+        } 
+      }
+    });
+
+    const expected = [
+      ["name", "Sam"],
+      ["favBook", [
+        ["title", "Blood Meridian"], 
+        ["author", [
+          ["name","Cormac McCarthy"]
+        ]] 
+      ]]
+    ];
+
+    expect(output).toEqual(expected);
+  });
+  
+  test(">1 nest level object; longest nest is in the middle", () => {
+    const output = deepEntries({
+      name: "Sam",
+      favBook: { 
+        title: "Blood Meridian", 
+        author: { 
+          name: "Cormac McCarthy" 
+        } 
+      },
+      pets: { 
+        name: "fido" 
+      }, 
+      testProp: "testVal"
+    });
+
+    const expected = [
+      ["name", "Sam"],
+      ["favBook", [
+        ["title", "Blood Meridian"], 
+        ["author", [
+          ["name","Cormac McCarthy"]
+        ]] 
+      ]],
+      ["pets", [
+        ["name", "fido"]
+      ]],
+      ["testProp", 'testVal']
+    ];
+
+    expect(output).toEqual(expected);
+  });
 });
--- sections/4-recursion.js
+++ sections/4-recursion.js
@@ -1,5 +1,19 @@
 
-const deepEntries = () => { };
+const deepEntries = (object) => {
+    const arrayifiedObject = [];
+
+    for (key in object) {
+        const value = object[key];
+
+        if ( typeof value === 'object' && !Array.isArray(value) ) {
+            arrayifiedObject.push([ key, deepEntries(value) ]);
+        } else {
+            arrayifiedObject.push([key, value]);
+        }
+    }
+
+    return arrayifiedObject;
+};
 
 const deeplyEquals = () => { };
 

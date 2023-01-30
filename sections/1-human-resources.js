c2af3b7ae92ebc2d63542208b94dd88ffef838ba
infoomics <sbx_94@hotmail.com>
2023-01-30 09:37:22 +0000

    first completion of 1-human-resources code

--- sections/1-human-resources.js
+++ sections/1-human-resources.js
@@ -1,4 +1,10 @@
-function removeAgents() {}
+function removeAgents(arr) {
+    if (arr.length === 0) return [];
+
+    const employees = JSON.parse( JSON.stringify(arr) );
+
+    return employees.filter((employee) => employee.profession !== 'mole');
+}
 
 function makeNameTags() {}
 

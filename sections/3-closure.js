b647c259e3d7a559d65ba459eb6efd3f004765ee
infoomics <sbx_94@hotmail.com>
2023-01-30 12:30:43 +0000

    added generateMultiples() and secureFunc() code

--- sections/3-closure.js
+++ sections/3-closure.js
@@ -1,5 +1,27 @@
-function generateMultiples() {}
+function generateMultiples(multiple) {
+    function incrementTimes(length) {
+        const multiples = [];
+        
+        for (let i = 1; i <= length; i++) {
+            multiples.push(multiple * i);
+        }
 
-function secureFunc() {}
+        return multiples;
+    }
+
+    return incrementTimes;
+}
+
+function secureFunc(realPassword, callback) {
+    function onValidate(passwordAttempt, ...args) {
+        if (passwordAttempt !== realPassword) return 'Sorry your password is incorrect!';
+        
+        if (args === undefined) return callback();
+
+        return callback.apply(null, [...args]);
+    }
+
+    return onValidate;
+}
 
 module.exports = { generateMultiples, secureFunc };

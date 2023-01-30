c028fa869219267485b5f42bbb1b00158f48eb5d
infoomics <sbx_94@hotmail.com>
2023-01-30 10:44:07 +0000

    added default properties and constructor

--- sections/2-oop-vending-machine.js
+++ sections/2-oop-vending-machine.js
@@ -1,3 +1,30 @@
-class VendingMachine {}
+class VendingMachine {
+    #credit = 0;
+    #stock = {
+        A : {},
+        B : {},
+        C : {},
+    };
+    
+    constructor() {
+
+    }
+
+    get credit() {
+        return this.#credit;
+    }
+
+    set credit(credit) {
+        this.#credit = credit;
+    }
+
+    get stock() {
+        return this.#stock;
+    }
+
+    set stock(credit) {
+        this.#stock = credit;
+    }
+}
 
 module.exports = VendingMachine;

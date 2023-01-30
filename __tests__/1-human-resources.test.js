e5d2ac4a0b06f580750d97345887d74f57944c2f
infoomics <sbx_94@hotmail.com>
2023-01-30 09:24:49 +0000

    added tests for removeAgents()

--- __tests__/1-human-resources.test.js
+++ __tests__/1-human-resources.test.js
@@ -4,6 +4,65 @@ const {
   removeAgents,
 } = require("../sections/1-human-resources.js");
 
-describe("function name here...", () => {
-  test("first test here...", () => {});
+describe("removeAgents()", () => {
+  test("empty array should return []", () => {
+      const input = [];
+      const consoleSpy = jest.spyOn(console, "log");
+
+      const output = removeAgents(input);
+
+      const expected = [];
+      expect(output).toEqual(expected);
+
+      // Some tests for side effects.
+      expect(input).not.toBe(output);
+      expect(consoleSpy).toHaveBeenCalledTimes(0);
+  });
+
+  test("one employee; with mole profession", () => {
+    const input = [{ name: "Mitch", profession: "mole" }];
+    const consoleSpy = jest.spyOn(console, "log");
+
+    const output = removeAgents(input);
+
+    const expected = [];
+    expect(output).toEqual(expected);
+
+    // Some tests for side effects.
+    expect(input).not.toBe(output);
+    expect(consoleSpy).toHaveBeenCalledTimes(0);
+  });
+
+  test("one employee; without mole profession", () => {
+    const input = [{ name: "Sam", profession: "artist" }];
+    const consoleSpy = jest.spyOn(console, "log");
+
+    const output = removeAgents(input);
+
+    const expected = [{ name: "Sam", profession: "artist" }];
+    expect(output).toEqual(expected);
+
+    // Some tests for side effects.
+    expect(input).not.toBe(output);
+    expect(consoleSpy).toHaveBeenCalledTimes(0);
+  });
+
+  test("multiple employees; with and without mole profession", () => {
+    const input = [
+      { name: "Sam", profession: "artist" },
+      { name: "Mitch", profession: "mole" },
+      { name: "Artiste Two", profession: "artist" },
+      { name: "Molee Two", profession: "mole" }
+    ];
+    const consoleSpy = jest.spyOn(console, "log");
+
+    const output = removeAgents(input);
+
+    const expected = [{ name: "Sam", profession: "artist" }, { name: "Artiste Two", profession: "artist" }];
+    expect(output).toEqual(expected);
+
+    // Some tests for side effects.
+    expect(input).not.toBe(output);
+    expect(consoleSpy).toHaveBeenCalledTimes(0);
+  });
 });
